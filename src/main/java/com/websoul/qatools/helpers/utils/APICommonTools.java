2cda25c9b347b17ce9a4769647c0b56441f19d66
Kalli Kavasi <kalli.kavasi@hellasdirect.gr>
2018-01-02 17:09:01 +0200

    added common tools for apis rest assured

--- src/main/java/com/websoul/qatools/helpers/utils/APICommonTools.java
+++ src/main/java/com/websoul/qatools/helpers/utils/APICommonTools.java
@@ -1,6 +1,7 @@
 package com.websoul.qatools.helpers.utils;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
 import com.websoul.qatools.helpers.context.ContextManagement;
 import com.websoul.qatools.helpers.context.cache.RuntimeCachedData;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +15,19 @@ public class APICommonTools {
     @Autowired
     ContextManagement cachedDataContext;
 
-    public void get(String uri){
-        RuntimeCachedData runtimeCachedData =(RuntimeCachedData)cachedDataContext.getObject("runtimeCachedData");
+    public void get(String uri) {
+        RuntimeCachedData runtimeCachedData = (RuntimeCachedData) cachedDataContext.getObject("runtimeCachedData");
         runtimeCachedData.setResponse(given().log().uri().when().get(uri));
         System.out.println("response: " + runtimeCachedData.getResponse().prettyPrint());
     }
 
-    public void post(JSONPObject json, String uri){
-        RuntimeCachedData runtimeCachedData =(RuntimeCachedData)cachedDataContext.getObject("runtimeCachedData");
+    public void post(JsonObject json, String uri) {
+        RuntimeCachedData runtimeCachedData = (RuntimeCachedData) cachedDataContext.getObject("runtimeCachedData");
         runtimeCachedData.setResponse(given().log().uri().when().contentType("application/json")
                 .body(json.toString())
                 .when()
                 .post(uri));
         System.out.println("response: " + runtimeCachedData.getResponse().prettyPrint());
-
     }
-
-
-
-
-
-
 
 }
