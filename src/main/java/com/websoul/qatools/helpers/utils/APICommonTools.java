8ad0f59f967251c4a36136b004e826523effb6f5
Kalli Kavasi <kalli.kavasi@hellasdirect.gr>
2018-01-03 13:33:06 +0200

    added logger in api common tools

--- src/main/java/com/websoul/qatools/helpers/utils/APICommonTools.java
+++ src/main/java/com/websoul/qatools/helpers/utils/APICommonTools.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.JsonObject;
 import com.websoul.qatools.helpers.context.ContextManagement;
 import com.websoul.qatools.helpers.context.cache.RuntimeCachedData;
+import com.websoul.qatools.steps.definitions.ConfigurationStepDefinitions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,12 @@ public class APICommonTools {
     @Autowired
     ContextManagement cachedDataContext;
 
+    private final Logger slf4jLogger = LoggerFactory.getLogger(ConfigurationStepDefinitions.class);
+
     public void get(String uri) {
         RuntimeCachedData runtimeCachedData = (RuntimeCachedData) cachedDataContext.getObject("runtimeCachedData");
         runtimeCachedData.setResponse(given().log().uri().when().get(uri));
-        System.out.println("response: " + runtimeCachedData.getResponse().prettyPrint());
+        slf4jLogger.info("response: " + runtimeCachedData.getResponse().prettyPrint());
     }
 
     public void post(JsonObject json, String uri) {
@@ -27,7 +32,7 @@ public class APICommonTools {
                 .body(json.toString())
                 .when()
                 .post(uri));
-        System.out.println("response: " + runtimeCachedData.getResponse().prettyPrint());
+        slf4jLogger.info("response: " + runtimeCachedData.getResponse().prettyPrint());
     }
 
 }
